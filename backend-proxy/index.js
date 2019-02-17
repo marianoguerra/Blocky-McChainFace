6cdc311b2c967fca274ee282ad57521c1deb44a7
Mariano Guerra <mariano@marianoguerra.org>
2019-02-17 08:06:46 +0100

    serve static files from backend-proxy

--- backend-proxy/index.js
+++ backend-proxy/index.js
@@ -12,6 +12,8 @@ const app = express(),
 
 app.use(express.json());
 app.use(cors(corsOptions));
+app.use('/service/', express.static('../service-ui/'));
+app.use('/', express.static('../user-ui/'));
 
 const ASSETS = {
   cars: {
